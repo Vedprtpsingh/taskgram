dc7f9f91257886b16941b14ea1ec45e3f5a280b3
vedprtpsingh <singhvedprartap14@gmail.com>
2025-07-10 23:26:45 +0530

    Updated project files

--- src/main/java/com/ved/taskgram/TaskgramApplication.java
+++ src/main/java/com/ved/taskgram/TaskgramApplication.java
@@ -1,13 +1,10 @@
 package com.ved.taskgram;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TaskgramApplication {
-
+// A Task Manager App
 	public static void main(String[] args) {
 		SpringApplication.run(TaskgramApplication.class, args);
 	}
-
 }
--- src/test/java/com/ved/taskgram/TaskgramApplicationTests.java
+++ src/test/java/com/ved/taskgram/TaskgramApplicationTests.java
@@ -9,5 +9,4 @@ class TaskgramApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
